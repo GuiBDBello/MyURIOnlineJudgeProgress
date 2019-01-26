64620d9104d2966604bbe870999144e152b8a7c2
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-25 23:46:28 -0200

    Iniciante: #1004 - Produto Simples

---
+++ 1004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int X, Y, PROD;
+    cin >> X >> Y;
+    
+    PROD = X * Y;
+    cout << "PROD = " << PROD << endl;
+ 
+    return 0;
+}
