b0405a306dd85410903d40a1aa6d8751fe0add16
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-27 02:36:08 -0200

    Iniciante: #1016 - Distância

---
+++ 1016.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double tempo;
+    int distancia;
+    cin >> distancia;
+    
+    tempo = distancia * 2;
+    cout << tempo << " minutos" << endl;
+    return 0;
+}
