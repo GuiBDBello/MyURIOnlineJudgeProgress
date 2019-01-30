fd1208a2c5345918a4449944f20dc8ad77052027
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-30 00:18:24 -0200

    Iniciante: #1041 - Coordenadas de um Ponto

---
+++ 1041.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    cout << fixed << setprecision(1);
+    
+    double x, y;
+    cin >> x >> y;
+    
+    if (x == 0.0 && y == 0.0) {
+        cout << "Origem" << endl;
+    } else if (x == 0.0) {
+        cout << "Eixo Y" << endl;
+    } else if (y == 0.0) {
+        cout << "Eixo X" << endl;
+    } else if (x > 0.0 && y > 0.0) {
+        cout << "Q1" << endl;
+    } else if (x < 0.0 && y > 0.0) {
+        cout << "Q2" << endl;
+    } else if (x < 0.0 && y < 0.0) {
+        cout << "Q3" << endl;
+    } else if (x > 0.0 && y < 0.0) {
+        cout << "Q4" << endl;
+    }
+    return 0;
+}
