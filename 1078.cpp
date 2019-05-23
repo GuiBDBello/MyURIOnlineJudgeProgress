a91b09aed2cdb33eca80bc0dacc39c85af991183
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-05-23 20:07:12 -0300

    Iniciante: #1078 - Tabuada

---
+++ 1078.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int N;
+    
+    cin >> N;
+ 
+    for(int i = 1; i <= 10; i++) {
+        cout << i << " x " << N << " = " << i * N << endl;
+    }
+    
+    return 0;
+}
