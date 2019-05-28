492b26d4999571c317190681ec45a1066e6ba992
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-05-27 22:35:37 -0300

    Iniciante: #1095 - Sequencia IJ 1

---
+++ 1095.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    
+    int i = 1, j;
+ 
+    for (j = 60; j >= 0; j-=5) {
+        cout << "I=" << i << " J=" << j << endl;
+        
+        i+=3;
+    }
+ 
+    return 0;
+}
